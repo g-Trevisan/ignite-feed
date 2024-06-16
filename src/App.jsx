ab249c68fc328e5f0083f0e9530892993a045d6c
g-Trevisan <gtrevis15@gmail.com>
2024-06-16 19:54:29 -0300

    Resolve merge conflicts

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Header } from './components/Header';
+import { Post } from './Post';
+import './styles.css';
+
+
+export function App() {
+
+  return (
+    <div>
+      <Header/>
+      <Post 
+        author="Gustavo Trevisan" 
+        content = "Lorem ipsum dolor sist amet consectetur adipisicing elit." 
+      />
+      <Post 
+        author="Rocket Seat" 
+        content = "Teste de conteudo para reaproveitamento de componente" 
+      />
+    </div>
+  )
+}
