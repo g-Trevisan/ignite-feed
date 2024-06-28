fd1c6fc7edeac6d089a842556e75da191bbecc9d
g-Trevisan <gtrevis15@gmail.com>
2024-06-27 22:43:54 -0300

    Criacao do componente de comentario

---
+++ src/components/Comment.jsx
@@ -0,0 +1,37 @@
+import { Trash } from 'phosphor-react'
+import { ThumbsUp } from "phosphor-react";
+
+import styles from './Comment.module.css'
+
+export function Comment(){
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/g-trevisan.png" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Gustavo Trevisan</strong>
+                            <time title="Publicado a 1h" dateTime="2024-26-06 23:19:58">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={20}/>
+                        </button>
+                    </header>
+
+                    <p>Muito bom Devon, parabéns!!!!!</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp size={20} />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+
+            </div>
+        </div>
+    )
+}
